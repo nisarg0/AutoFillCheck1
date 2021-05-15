42dc7b77ea2cf5d49acf710316d3cdb268e755f6
nisarg0 <nisarggogate212@gmail.com>
2021-05-15 10:33:12 +0530

    ketan_discussion1

--- client.js
+++ client.js
@@ -1,14 +1,33 @@
-function sendMsgToExtension() {
-	console.log("In extension");
-	if (chrome && chrome.runtime && chrome.runtime.sendMessage) {
-		chrome.runtime.sendMessage(
-			"feneepekkegnbffjepdhkdiggmebjifo",
-			{ greeting: "yes" },
-			onAccessApproved
-		);
-	} else {
-		console.log("No chrome");
-	}
-}
+// function sendMsgToExtension() {
+// 	console.log("In extension");
+// 	if (chrome && chrome.runtime && chrome.runtime.sendMessage) {
+// 		chrome.runtime.sendMessage(
+// 			"feneepekkegnbffjepdhkdiggmebjifo",
+// 			{ greeting: "yes" },
+// 			onAccessApproved
+// 		);
+// 	} else {
+// 		console.log("No chrome");
+// 	}
+// }
 
-document.getElementById("myBtn").addEventListener("click", sendMsgToExtension);
+window.onload = function () {
+	document.getElementById("myBtn").addEventListener("click", function () {
+		console.log("Sending info to extension");
+
+		if (chrome && chrome.runtime && chrome.runtime.sendMessage) {
+			chrome.runtime.sendMessage(
+				"feneepekkegnbffjepdhkdiggmebjifo",
+				{ greeting: "yes" },
+				onAccessApproved
+			);
+		} else {
+			console.log("No chrome");
+		}
+	});
+};
+
+// window.addEventListener('load',function(){
+// 	document.getElementById("id1").addEventListener("click", click_handler1, false);
+// 	document.getElementById("id2").addEventListener("click", click_handler2, false);
+// });
