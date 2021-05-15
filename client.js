d7b5da42ec806879aca67a9c75d1a8c2800e6498
nisarg0 <nisarggogate212@gmail.com>
2021-05-15 12:00:25 +0530

    trying via event binding

--- client.js
+++ client.js
@@ -10,34 +10,50 @@
 // 		console.log("No chrome");
 // 	}
 // }
+
 const url = "https://www.facebook.com/";
-
 window.onload = function () {
-	document.getElementById("myBtn").addEventListener("click", function () {
-		console.log("Sending info to extension");
-
-		// if (chrome) console.log("chrome is defined");
-
-		// chrome.runtime.sendMessage(
-		// 	editorExtensionId,
-		// 	{ openUrlInEditor: url },
-		// 	function (response) {
-		// 		console.log("msg sent");
-		// 		if (!response.success) handleError(url);
-		// 	}
-		// );
-
-		// if (chrome && chrome.runtime && chrome.runtime.sendMessage) {
-		// 	chrome.runtime.sendMessage(
-		// 		"feneepekkegnbffjepdhkdiggmebjifo",
-		// 		{ greeting: "yes" },
-		// 		onAccessApproved
-		// 	);
-		// } else {
-		// 	console.log("No chrome");
-		// }
-	});
+	document.getElementById("theButton").addEventListener(
+		"click",
+		function () {
+			window.postMessage(
+				{
+					type: "FROM_PAGE_TO_CONTENT_SCRIPT",
+					text: "Hello from the webpage!",
+				},
+				"*"
+			);
+		},
+		false
+	);
 };
+
+// window.onload = function () {
+// 	document.getElementById("myBtn").addEventListener("click", function () {
+// 		console.log("Sending info to extension");
+
+// 		// if (chrome) console.log("chrome is defined");
+
+// 		// chrome.runtime.sendMessage(
+// 		// 	editorExtensionId,
+// 		// 	{ openUrlInEditor: url },
+// 		// 	function (response) {
+// 		// 		console.log("msg sent");
+// 		// 		if (!response.success) handleError(url);
+// 		// 	}
+// 		// );
+
+// 		// if (chrome && chrome.runtime && chrome.runtime.sendMessage) {
+// 		// 	chrome.runtime.sendMessage(
+// 		// 		"feneepekkegnbffjepdhkdiggmebjifo",
+// 		// 		{ greeting: "yes" },
+// 		// 		onAccessApproved
+// 		// 	);
+// 		// } else {
+// 		// 	console.log("No chrome");
+// 		// }
+// 	});
+// };
 
 // window.addEventListener('load',function(){
 // 	document.getElementById("id1").addEventListener("click", click_handler1, false);
