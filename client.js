0999c8a4137db2bb4ae91421c1c7dff824403262
nisarg0 <nisarggogate212@gmail.com>
2021-05-26 15:26:23 +0530

    testingon icici bank

--- client.js
+++ client.js
@@ -17,7 +17,7 @@ function genMessage(
 async function getDetails(id) {
 	// Will get element by id
 	Message = genMessage(
-		"https://retail.onlinesbi.com/retail/login.htm",
+		"https://infinity.icicibank.com/corp/AuthenticationController?FORMSGROUP_ID__=AuthenticationFG&__START_TRAN_FLAG__=Y&FG_BUTTONS__=LOAD&ACTION.LOAD=Y&AuthenticationFG.LOGIN_FLAG=1&BANK_ID=ICI&ITM=nli_personalb_personal_login_btn&_ga=2.136565301.449586349.1622022497-455473517.1622022497&_gl=1*c0o5z3*_ga*NDU1NDczNTE3LjE2MjIwMjI0OTc.*_ga_SKB78GHTFV*MTYyMjAyMjQ5OC4xLjEuMTYyMjAyMjkzMy42MA..",
 		btoa("nisarg"),
 		btoa("12345678"),
 		"DUMMY1",
--- script.sql
+++ script.sql
@@ -8,4 +8,8 @@ INSERT INTO websitedetails (url,userNameField,passField,isActive,isDeleted,Added
 VALUES ('https://retail.onlinesbi.com/retail/login.htm','username','label2',1,0,'Mahesh',now(),'Mahesh',now());
 
 INSERT INTO websitedetails (url,userNameField,passField,isActive,isDeleted,AddedBy,AddedDate,ModifiedBy,ModifiedDate) 
-VALUES ('https://retail.onlinesbi.com/retail/login.htm','DUMMY1','dummypwd1',1,0,'Mahesh',now(),'Mahesh',now());
+VALUES ('https://infinity.icicibank.com/corp/AuthenticationController?FORMSGROUP_ID__
+=AuthenticationFG&__START_TRAN_FLAG__=Y&FG_BUTTONS__=LOAD&ACTION.LOAD=Y&AuthenticationFG.
+LOGIN_FLAG=1&BANK_ID=ICI&ITM=nli_personalb_personal_login_btn&_ga=2.136565301.449586349.
+1622022497-455473517.1622022497&_gl=1*c0o5z3*_ga*NDU1NDczNTE3LjE2MjIwMjI0OTc.*
+_ga_SKB78GHTFV*MTYyMjAyMjQ5OC4xLjEuMTYyMjAyMjkzMy42MA..','DUMMY1','dummypwd1',1,0,'Mahesh',now(),'Mahesh',now());
